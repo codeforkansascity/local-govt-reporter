6bac1862778271ddfcd9ea9e45b49872cdb5cdc2
Adam Bungart <abungart@gmail.com>
2020-11-23 22:47:29 -0600

    Set up basic configuration for Meeting Card

--- src/components/MeetingCard.js
+++ src/components/MeetingCard.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const MeetingCard = () => {
-  return <article class="slds-card"></article>;
+  return (
+    <article class="slds-card">
+      <header class="slds-card__header">
+        <h2 class="slds-card__header-title">Card Header</h2>
+      </header>
+      <div class="slds-card__body slds-card__body_inner">
+        <div style={{ display: "inline-block" }}>This is in one div</div>
+        <div style={{ display: "inline-block" }}>This is in the second div</div>
+      </div>
+      <footer class="slds-card__footer">View All Info Button</footer>
+    </article>
+  );
 };
 
 export default MeetingCard;
