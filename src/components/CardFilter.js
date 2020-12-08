71a86b7039f5b6078b2d0468cae4eed46961ac16
Adam Bungart <abungart@gmail.com>
2020-12-07 19:21:10 -0600

    Changed tabs to radio buttons on CardFilter

--- src/components/CardFilter.js
+++ src/components/CardFilter.js
@@ -1,102 +1,142 @@
-import React from "react";
+import React, { Component } from "react";
 import MeetingCard from "./MeetingCard";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 import Button from "@salesforce/design-system-react/components/button";
-import Tabs from "@salesforce/design-system-react/components/tabs";
-import TabsPanel from "@salesforce/design-system-react/components/tabs/panel";
+import RadioButtonGroup from "@salesforce/design-system-react/components/radio-button-group";
+import Radio from "@salesforce/design-system-react/components/radio-button-group/radio";
 
-const CardFilter = () => {
-  const state = {
-    filterSelect: "Region",
+class CardFilter extends Component {
+  state = {
+    filterSelect: "Date",
     local: "Missouri",
   };
 
-  return (
-    <div style={{ width: "1236px", marginLeft: "auto", marginRight: "auto" }}>
-      <article
-        class="slds-card"
-        style={{
-          margin: "20px",
-          float: "left",
-          width: "814px",
-          height: "256px",
-        }}
-      >
-        <header class="slds-card__header">
-          <h1
-            style={{
-              fontSize: "30px",
-              fontFamily: "Merriweather",
-              fontWeight: "bold",
-            }}
-          >
-            Find local meetings and meeting minutes
-          </h1>
-        </header>
-        <div class="slds-card__body slds-card__body_inner">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
-            porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et
-            faucibus arcu condimentum sed.Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat
-            volutpat. Donec placerat nisl magna, et faucibus.
-          </p>
-          <footer>
-            <Button
-              class="slds-button slds-button_brand "
-              href="javascript:void(0);"
-              variant="outline-brand"
+  constructor(props) {
+    super(props);
+    this.state = { checked: "Date" };
+  }
+
+  render() {
+    const filter = ["Date", "Jurisdiction"];
+
+    return (
+      <div style={{ width: "1236px", marginLeft: "auto", marginRight: "auto" }}>
+        <article
+          class="slds-card"
+          style={{
+            margin: "20px",
+            float: "left",
+            width: "814px",
+            height: "256px",
+          }}
+        >
+          <header class="slds-card__header">
+            <h1
               style={{
-                position: "absolute",
-                bottom: "15px",
-                left: "15px",
-                margin: "15px",
-                fontSize: "12pt",
+                fontSize: "30px",
+                fontFamily: "Merriweather",
+                fontWeight: "bold",
               }}
             >
-              Need more help?
-            </Button>
-          </footer>
-        </div>
-      </article>
-      <article
-        class="slds-card"
-        style={{
-          float: "right",
-          width: "339px",
-          height: "364px",
-          margin: "20px",
-        }}
-      >
-        <header class="slds-card__header">
-          <h1
+              Find local meetings and meeting minutes
+            </h1>
+          </header>
+          <div class="slds-card__body slds-card__body_inner">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              at porttitor sem. Aliquam erat volutpat. Donec placerat nisl
+              magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Aliquam at porttitor sem.
+              Aliquam erat volutpat. Donec placerat nisl magna, et faucibus.
+            </p>
+            <footer>
+              <Button
+                class="slds-button slds-button_brand "
+                href="javascript:void(0);"
+                variant="outline-brand"
+                style={{
+                  position: "absolute",
+                  bottom: "15px",
+                  left: "15px",
+                  margin: "15px",
+                  fontSize: "12pt",
+                }}
+              >
+                Need more help?
+              </Button>
+            </footer>
+          </div>
+        </article>
+        <article
+          class="slds-card"
+          style={{
+            float: "right",
+            width: "339px",
+            height: "364px",
+            margin: "20px",
+          }}
+        >
+          <header class="slds-card__header">
+            <h1
+              style={{
+                fontSize: "20px",
+                fontFamily: "Merriweather",
+                fontWeight: "bold",
+              }}
+            >
+              Upcoming Meetings
+            </h1>
+          </header>
+          <Calendar />
+        </article>
+
+        <article
+          class="slds-card"
+          style={{ margin: "20px", float: "left", width: "814px" }}
+        >
+          <div
             style={{
               fontSize: "20px",
               fontFamily: "Merriweather",
-              fontWeight: "bold",
+              float: "left",
+              margin: "15px",
             }}
           >
-            Upcoming Meetings
-          </h1>
-        </header>
-        <Calendar />
-      </article>
-      <article
-        class="slds-card"
-        style={{ margin: "20px", float: "left", width: "814px" }}
-      >
-        Sort Meetings By
-        <Tabs indicatorColor="primary" textColor="primary" centered>
-          <TabsPanel label="Region"></TabsPanel>
-          <TabsPanel label="Date"></TabsPanel>
-        </Tabs>
-      </article>
+            Sort meetings by
+          </div>
+          <RadioButtonGroup
+            onChange={(event) => this.setState({ checked: event.target.value })}
+            disabled={this.props.disabled}
+            required={this.props.required}
+            name={this.props.name}
+            errorId={this.props.errorId}
+            style={{ float: "left", margin: "15px" }}
+          >
+            {filter.map((filter) => (
+              <Radio
+                key={filter}
+                id={filter}
+                labels={{ label: filter }}
+                value={filter}
+                checked={this.state.checked === filter}
+                variant="button-group"
+              />
+            ))}
+          </RadioButtonGroup>
+          <div style={{ float: "left" }}>
+            <p style={{ fontStyle: "italic" }}>
+              See any inaccuracies in our information?
+            </p>
+            <p>Let us know</p>
+          </div>
+        </article>
 
-      <MeetingCard />
-    </div>
-  );
-};
+        <MeetingCard />
+      </div>
+    );
+  }
+}
 
 export default CardFilter;
