239f89a5d3211a2a481646a60aea1d37740c19b7
Adam Bungart <abungart@gmail.com>
2020-12-14 14:46:39 -0600

    Added Jurisdiction Filter

--- src/components/CardFilter.js
+++ src/components/CardFilter.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import JurisdictionFilter from "./JurisdictionFilter";
+
 import RadioButtonGroup from "@salesforce/design-system-react/components/radio-button-group";
 import Radio from "@salesforce/design-system-react/components/radio-button-group/radio";
 
@@ -14,47 +16,49 @@ class CardFilter extends Component {
 
     return (
       <article
-        className="slds-card slds-grid slds-gutters_direct"
+        className="slds-card"
         style={{ margin: "20px", float: "left", width: "814px" }}
       >
-        <div
-          className="slds-col"
-          style={{
-            fontSize: "20px",
-            fontFamily: "Merriweather",
-            float: "left",
-            margin: "15px",
-            width: "220px",
-          }}
-        >
-          Sort meetings by
+        <div className="slds-grid slds-gutters_direct">
+          <div
+            className="slds-col"
+            style={{
+              fontSize: "20px",
+              fontFamily: "Merriweather",
+              float: "left",
+              margin: "15px",
+              width: "220px",
+            }}
+          >
+            Sort meetings by
+          </div>
+          <RadioButtonGroup
+            className="slds-col"
+            onChange={(event) => this.setState({ checked: event.target.value })}
+            style={{ float: "left", margin: "15px", width: "166px" }}
+          >
+            {filter.map((filter) => (
+              <Radio
+                key={filter}
+                id={filter}
+                labels={{ label: filter }}
+                value={filter}
+                checked={this.state.checked === filter}
+                variant="button-group"
+              />
+            ))}
+          </RadioButtonGroup>
+          <div
+            className="slds-col"
+            style={{ float: "left", width: "250px", margin: "15px" }}
+          >
+            <p style={{ fontStyle: "italic", textAlign: "left" }}>
+              See any inaccuracies in our information?
+            </p>
+            <p style={{ textAlign: "left" }}>Let us know</p>
+          </div>
         </div>
-        <RadioButtonGroup
-          className="slds-col"
-          onChange={(event) => this.setState({ checked: event.target.value })}
-          style={{ float: "left", margin: "15px", width: "166px" }}
-        >
-          {filter.map((filter) => (
-            <Radio
-              key={filter}
-              id={filter}
-              labels={{ label: filter }}
-              value={filter}
-              checked={this.state.checked === filter}
-              variant="button-group"
-            />
-          ))}
-        </RadioButtonGroup>
-        <div
-          className="slds-col"
-          style={{ float: "left", width: "250px", margin: "15px" }}
-        >
-          <p style={{ fontStyle: "italic", textAlign: "left" }}>
-            See any inaccuracies in our information?
-          </p>
-          <p style={{ textAlign: "left" }}>Let us know</p>
-        </div>
-        {this.state.checked === "Jurisdiction" && <div>In Jurisdiction</div>}
+        {this.state.checked === "Jurisdiction" && <JurisdictionFilter />}
       </article>
     );
   }
---
+++ src/components/JurisdictionFilter.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+import RadioButtonGroup from "@salesforce/design-system-react/components/radio-button-group";
+import Radio from "@salesforce/design-system-react/components/radio-button-group/radio";
+
+class JurisdictionFilter extends Component {
+  render() {
+    return <div>In Jurisdiction</div>;
+  }
+}
+
+export default JurisdictionFilter;
