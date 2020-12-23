498568627860f3b85242160be34d52f8e0ee297e
Adam Bungart <abungart@gmail.com>
2020-12-22 22:18:34 -0600

    Added placeholder content for formatting about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,50 @@ const About = () => {
     <div style={{ width: "1236px", marginLeft: "auto", marginRight: "auto" }}>
       <div>
         <h1>Down in San Francisco</h1>
+        <p>
+          Government cannot go unchecked in a democracy. That’s why we worked
+          with the team of civic coders at KC Digital Drive to develop
+          ___________, a community-designed effort to standardize, collect and
+          display public meeting information, including dates, times, locations
+          and documentation, in a central database serving Kansas and Missouri.
+          _________ equips journalists with public records to report local news
+          and residents with information to effect change in their communities.{" "}
+        </p>
       </div>
       <div>
         <h1>Why we deliver...</h1>
+        <div>
+          _________ equips journalists with public records to report local news
+          and residents with information to effect change in their communities.
+        </div>
+        <div>
+          Allows the public to have easier access to the government records,
+          enabling easier civic participation.
+        </div>
+        <div>
+          _________ equips journalists with public records to report local news
+          and residents with information to effect change in their communities.{" "}
+        </div>
+        <div>
+          Allows the public to have easier access to the government records,
+          enabling easier civic participation.
+        </div>
       </div>
       <div>
         <h1>Our Partners</h1>
+        <div>
+          <p>
+            Identifying a shared interest in illuminating civic processes to
+            preserve democracy, KC Digital Drive joined the effort as a
+            technology partner.
+          </p>
+        </div>
+        <div>
+          Jackson County Advocate The Kansas City Star Kansas Press Association
+          Columbia Missourian at the University of Missouri School of Journalism
+          KCUR KLC Journal Northeast News Shawnee Mission Post and Startland
+          News. KC Digital Drive
+        </div>
       </div>
       <footer></footer>
     </div>
