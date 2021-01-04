c64d56ed04fcd29fbdcf250e97dcd4190d1b9010
Adam Bungart <abungart@gmail.com>
2021-01-04 15:04:02 -0600

    Added Cards to CityInfo

--- src/components/CityInfo.js
+++ src/components/CityInfo.js
@@ -3,10 +3,16 @@ import React from "react";
 const CityInfo = () => {
   return (
     <div style={{ width: "1236px", marginLeft: "auto", marginRight: "auto" }}>
-      <div>
-        <h1>Kansas City</h1>
-        <p>Next meeting</p>
-      </div>
+      <article className="slds-card">
+        <div>Kansas City, MO</div>
+        <div>Meet the Council</div>
+      </article>
+      <article className="slds-card">
+        <h1>Upcoming Meetings</h1>
+      </article>
+      <article className="slds-card">
+        <h1>Past Meetings</h1>
+      </article>
     </div>
   );
 };
