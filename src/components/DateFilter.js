fcada378d81a95776543919c59f1a3bba1dff683
Adam Bungart <abungart@gmail.com>
2021-01-13 16:48:25 -0600

    Added first datepicker for sort

--- src/components/DateFilter.js
+++ src/components/DateFilter.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import moment from "moment";
+
+import Datepicker from "@salesforce/design-system-react/components/date-picker";
 
 class DateFilter extends Component {
+  state = {
+    value: undefined,
+  };
   render() {
+    const handleChange = (event, data) => {
+      this.setState({ value: data.date });
+    };
+
     return (
       <div className="slds-grid slds-gutters_direct">
         <div
@@ -15,6 +25,37 @@ class DateFilter extends Component {
         >
           Start Date
         </div>
+        <Datepicker
+          labels={{
+            label: "Date",
+          }}
+          onChange={(event, data) => {
+            this.handleChange(event, data);
+
+            if (this.props.action) {
+              const dataAsArray = Object.keys(data).map((key) => data[key]);
+              this.props.action("onChange")(event, data, ...dataAsArray);
+            } else if (console) {
+              console.log("onChange", event, data);
+            }
+          }}
+          onCalendarFocus={(event, data) => {
+            if (this.props.action) {
+              const dataAsArray = Object.keys(data).map((key) => data[key]);
+              this.props.action("onCalendarFocus")(event, data, ...dataAsArray);
+            } else if (console) {
+              console.log("onCalendarFocus", event, data);
+            }
+          }}
+          formatter={(date) => {
+            return date ? moment(date).format("M/D/YYYY") : "";
+          }}
+          parser={(dateString) => {
+            return moment(dateString, "MM-DD-YYYY").toDate();
+          }}
+          value={this.state.value}
+        />
+
         <div
           className="slds-col"
           style={{
