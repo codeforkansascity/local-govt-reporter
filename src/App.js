07d450db0e0fc8b1305eb790834f6fb5e6b05900
Jake LaCombe <jake.lacombe2@gmail.com>
2021-01-24 20:52:37 -0600

    Incorporate app with API

--- src/App.js
+++ src/App.js
@@ -11,22 +11,27 @@ import TopInfoBar from "./components/TopInfoBar";
 import About from "./components/About";
 import CityInfo from "./components/CityInfo";
 import NavBar from "./components/NavBar";
+import { QueryClient, QueryClientProvider } from 'react-query'
+ 
+ const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <div className="App">
-        <Switch>
-          <Redirect exact from="/" to="/home" />
-          <Route exact path="/home" component={TopInfoBar} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/info" component={CityInfo} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <NavBar />
+        <div className="App">
+          <Switch>
+            <Redirect exact from="/" to="/home" />
+            <Route exact path="/home" component={TopInfoBar} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/info" component={CityInfo} />
 
-          <Route render={() => <h1>404</h1>} />
-        </Switch>
-      </div>
-    </Router>
+            <Route render={() => <h1>404</h1>} />
+          </Switch>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
