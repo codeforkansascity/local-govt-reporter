44ad88877ff655d6ca5710be7d0806da9f6f2ae0
Adam Bungart <abungart@gmail.com>
2021-03-14 17:38:46 -0500

    Added styles and content to CityInfo header

--- src/components/CityInfo.tsx
+++ src/components/CityInfo.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import CityInfoMeeting from './CityInfoMeeting';
 
+import { Button } from '@salesforce/design-system-react';
+
 const CityInfo = () => {
   return (
     <div style={{ width: '1236px', marginLeft: 'auto', marginRight: 'auto' }}>
-      <article className='slds-card'>
-        <div>Kansas City, MO</div>
-        <article className='slds-card slds-size_1-of-2'>
+      <article
+        className='slds-card'
+        style={{ display: 'inline-block', width: '100%', height: '265px' }}
+      >
+        <article
+          className='slds-card slds-size_1-of-2'
+          style={{ float: 'left', paddingLeft: '15px' }}
+        >
+          <h1 style={{ fontSize: '26pt', textAlign: 'left', padding: '15px' }}>
+            Kansas City, MO
+          </h1>
+          <p style={{ fontSize: '12pt', textAlign: 'left', padding: '15px' }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
+            porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et
+            faucibus arcu condimentum sed.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit.
+          </p>
+          <Button
+            className='slds-button slds-button_brand '
+            variant='outline-brand'
+            style={{
+              color: 'blue',
+              position: 'absolute',
+              bottom: '15px',
+              left: '15px',
+              margin: '15px',
+              fontSize: '12pt',
+            }}
+          >
+            Visit City Council Website
+          </Button>
+        </article>
+        <article
+          className='slds-card slds-size_1-of-2'
+          style={{ float: 'right' }}
+        >
           Meet the Council
         </article>
       </article>
